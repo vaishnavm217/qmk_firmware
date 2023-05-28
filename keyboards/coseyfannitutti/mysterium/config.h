13f0925b24c35dbdbcbbea82e85ff2a55e189290
vaishnavm217 <vaishnavmurali@gmail.com>
2023-05-28 22:08:55 +0530

    Added klien keyboard and CI/CD
    Signed-off-by: vaishnavm217 <vaishnavmurali@gmail.com>

--- keyboards/coseyfannitutti/mysterium/config.h
+++ keyboards/coseyfannitutti/mysterium/config.h
@@ -16,6 +16,17 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
+#include "config_common.h"
+
+#define VENDOR_ID       0x6B62
+#define PRODUCT_ID      0x8719
+#define DEVICE_VER      0x0001
+#define MANUFACTURER    coseyfannitutti
+#define PRODUCT         MYSTERIUM
+
+/* key matrix size */
+#define MATRIX_ROWS 6
+#define MATRIX_COLS 18
 
 /*
  * Keyboard Matrix Assignments
@@ -34,23 +45,53 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* COL2ROW, ROW2COL*/
 #define DIODE_DIRECTION COL2ROW
 
+/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
+#define DEBOUNCE 5
+
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
 /* Locking resynchronize hack */
 #define LOCKING_RESYNC_ENABLE
 
-/*
- * Feature disable options
- *  These options are also useful to firmware size reduction.
- */
+/* Bootmagic Lite key configuration */
+#define BOOTMAGIC_LITE_ROW 0
+#define BOOTMAGIC_LITE_COLUMN 0
 
-/* disable debug print */
-//#define NO_DEBUG
 
-/* disable print */
-//#define NO_PRINT
 
-/* disable action features */
-//#define NO_ACTION_LAYER
-//#define NO_ACTION_TAPPING
-//#define NO_ACTION_ONESHOT
+#define OLED_TIMEOUT 100000
+
+#define RGB_DI_PIN B5
+#define RGBLED_NUM 18
+#define RGBLIGHT_ANIMATIONS
+ #define RGBLIGHT_HUE_STEP 8
+#define RGBLIGHT_SAT_STEP 8
+#define RGBLIGHT_VAL_STEP 8
+#define RGBLIGHT_LIMIT_VAL 150 /* The maximum brightness level */
+#define RGBLIGHT_SLEEP  /* If defined, the RGB lighting will be switched off when the host goes to sleep */
+/*== all animations enable ==*/
+//   #define RGBLIGHT_ANIMATIONS
+/*== or choose animations ==*/
+//   #define RGBLIGHT_EFFECT_BREATHING
+#define RGBLIGHT_EFFECT_RAINBOW_MOOD
+#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+//   #define RGBLIGHT_EFFECT_SNAKE
+#define RGBLIGHT_EFFECT_KNIGHT
+//   #define RGBLIGHT_EFFECT_CHRISTMAS
+#define RGBLIGHT_EFFECT_STATIC_GRADIENT
+//   #define RGBLIGHT_EFFECT_RGB_TEST
+//   #define RGBLIGHT_EFFECT_ALTERNATING
+/*== customize breathing effect ==*/
+/*==== (DEFAULT) use fixed table instead of exp() and sin() ====*/
+#define RGBLIGHT_BREATHE_TABLE_SIZE 256      // 256(default) or 128 or 64
+/*==== use exp() and sin() ====*/
+#define RGBLIGHT_EFFECT_BREATHE_CENTER 1.85  // 1 to 2.7
+#define RGBLIGHT_EFFECT_BREATHE_MAX    255   // 0 to 255
+
+
+
+    // RGB Matrix Animation modes. Explicitly enabled
+// For full list of effects, see:
+// https://docs.qmk.fm/#/feature_rgb_matrix?id=rgb-matrix-effects
+
+// enabled only of RGB_MATRIX_KEYPRESSES or RGB_MATRIX_KEYRELEASES is defined
